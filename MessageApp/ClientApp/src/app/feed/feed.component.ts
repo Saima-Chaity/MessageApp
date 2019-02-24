0bf0ac5d9ff1f33897eca9b040770346bad1fc4e
Saima Chaity <saima.s.chaity@gmail.com>
2019-02-23 22:00:30 -0800

    Removed send message form and passed the array of messages to message component

--- MessageApp/ClientApp/src/app/feed/feed.component.ts
+++ MessageApp/ClientApp/src/app/feed/feed.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnChanges  } from '@angular/core';
 import { ChatService } from '../services/chat.service';
 import { AuthService } from '../services/auth.service';
+import { ChatMessage } from '../models/chatMessage.model';
 import { Router } from '@angular/router';
 
 
@@ -9,8 +10,7 @@ import { Router } from '@angular/router';
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnInit {
-
+export class FeedComponent implements OnInit, OnChanges  {
   message: string;
   file: string;
   url: any;
@@ -20,6 +20,8 @@ export class FeedComponent implements OnInit {
   messageSender: any = [];
   messageReceiver: any = [];
   userId: any;
+
+  feed: Array<ChatMessage>;
 
   constructor(private chatService: ChatService, private authService: AuthService, private router: Router) {
     //this.userId = sessionStorage.getItem('userId');
@@ -33,28 +35,67 @@ export class FeedComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getAllMessages();
+    this.chatService.GetAllMessage().subscribe(
+      data => {
+        this.feed = data.json();
+        console.log(this.feed);
+      }
+    );
   }
 
-  getAllMessages() {
-    this.chatService.GetAllMessage().subscribe(data =>
-      this.allMessages = data.json()
-    )
+  ngOnChanges() {
+    this.chatService.GetAllMessage().subscribe(
+      data => {
+        this.feed = data.json();
+        console.log(this.feed);
+      }
+    );
   }
 
-  send() {
-    if (this.message && !this.file) {
-      this.chatService.sendMessage(this.message).subscribe(data => {
-        this.getAllMessages();
-      });
-      this.message = '';
-    }
-  }
+  //ngOnInit(chatMessage = this.chatMessage) {
+  //  this.url = chatMessage.file;
+  //  this.getAllMessages();
+  //}
 
-  handleSubmit(event: any) {
-    if (event.keyCode === 13) {
-      this.send();
-    }
-  }
+  //onFileChanged(event) {
+  //  this.url = event.target.files[0];
+  //  const reader = new FileReader();
+  //  reader.readAsDataURL(event.target.files[0]);
+  //  reader.onload = (e) => {
+  //    this.url = (<FileReader>e.target).result;
+  //    this.file = reader.result.split(',')[1];
+  //  };
+  //}
+
+
+  //getAllMessages() {
+  //  this.chatService.GetAllMessage().subscribe(data => {
+  //    console.log(JSON.stringify(data.json()));
+  //    this.allMessages = data.json();
+  //    this.url = data.json()["sendFile"];
+  //  });
+  //}
+
+  //send() {
+  //  if (this.message && !this.file) {
+  //    this.chatService.sendMessage(this.message).subscribe(data => {
+  //      this.getAllMessages();
+  //    });
+  //    this.message = '';
+  //  }
+  //  if (this.message && this.file) {
+  //    this.chatService.sendMessageAndFile(this.message, this.file).subscribe(data => {
+  //      this.getAllMessages();
+  //    });
+  //    this.message = '';
+  //    this.file = '';
+  //  }
+  //}
+
+  //handleSubmit(event: any) {
+  //  if (event.keyCode === 13) {
+  //    this.send();
+  //  }
+  //}
 
 }
